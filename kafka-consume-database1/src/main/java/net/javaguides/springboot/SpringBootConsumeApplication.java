22d3bc4ed90484a9cef75b139e1bce08f90c468f
mounikasika <mounika.sika8@gmail.com>
2024-05-03 22:43:38 -0500

    Checking in the project changes. This doesn't have the changes for adding the events to mysql.

---
+++ kafka-consume-database1/src/main/java/net/javaguides/springboot/SpringBootConsumeApplication.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class SpringBootConsumeApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootConsumeApplication.class);
+    }
+}
